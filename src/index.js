c63519d3f1975a904fc2c13a22051b23d04c6641
Stenio Wagner <stenio.wagner1@gmail.com>
2019-02-09 20:32:34 -0300

    Setting up environment and support tools

---
+++ src/index.js
@@ -0,0 +1,21 @@
+// @flow
+
+import React from 'react';
+
+import { View, Text } from 'react-native';
+
+const App = (): Object => (
+  <View
+    style={{
+      width: '100%',
+      height: '100%',
+      justifyContent: 'center',
+      alignItems: 'center',
+      backgroundColor: '#f0f',
+    }}
+  >
+    <Text>BJS</Text>
+  </View>
+);
+
+export default App;
