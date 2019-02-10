82c9b4d2043b557c12385073671254f5d4a3561d
Stenio Wagner <stenio.wagner1@gmail.com>
2019-02-10 13:20:29 -0300

    Setting up Flow

--- src/Teste.js
+++ src/Teste.js
@@ -4,18 +4,24 @@ import React from 'react';
 
 import { View, Text } from 'react-native';
 
-const App = (): Object => (
-  <View
-    style={{
-      width: '100%',
-      height: '100%',
-      justifyContent: 'center',
-      alignItems: 'center',
-      backgroundColor: '#f0f',
-    }}
-  >
-    <Text>BJS</Text>
-  </View>
-);
+const sum = (n1: number, n2: number): number => n1 + n2;
+
+const App = (): Object => {
+  sum(1, '1');
+
+  return (
+    <View
+      style={{
+        width: '100%',
+        height: '100%',
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#f0f',
+      }}
+    >
+      <Text>BJS</Text>
+    </View>
+  );
+};
 
 export default App;
